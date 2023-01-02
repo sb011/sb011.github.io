ec047751c2c13db65b50abd46ab2cab2c0b3b2e5
Smit <smitbhoraniya11@gmail.com>
2023-01-02 11:58:39 +0530

    keywords update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,9 @@ import SEO from "@bradgarropy/next-seo"
 export default function App({ Component, pageProps }: AppProps) {
   return <>
     <SEO 
-      title="Smit&apos;s Portfolio"
+      title="Smit Bhoraniya"
       description="Freelancer, learner, and creative developer living in Gujarat."
-      keywords={["smit", "smit's portfolio", "portfolio", "smit bhoraniya", "bhoraniya smit", "sb011"]}
+      keywords={["smit", "smit's portfolio", "portfolio", "smit bhoraniya", "bhoraniya smit", "sb011", "portfolio websites", "top portfolio websites", "best portfolio websites", "best portfolio", "smit portfolio", "best portfolio design", "portfolio design", "portfolio design ideas", ]}
       icon="/SB.ico"
       facebook={{
         image: "/profile_pic.webp",
