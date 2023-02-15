52c6d180140f0fbdaab26bfc1926ed6a46befb84
Smit <smitbhoraniya11@gmail.com>
2023-02-16 00:55:25 +0530

    Experience UI changed

--- components/experience.tsx
+++ components/experience.tsx
@@ -3,53 +3,116 @@ import Image from 'next/image'
 import ImgSrc from '../public/Amazon_icon.svg'
 import ImgSrc2 from '../public/jp-morgan-chase.svg'
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 const Experience = () => {
+    const main = {
+        init: {
+            scale: 0.5,
+            y: 100
+        },
+        inView: {
+            scale: 1,
+            y: 0,
+            transition: {
+                duration: 0.5,
+                staggerChildren: 0.1,
+                ease: "easeInOut",
+                when: "beforeChildren"
+            }
+        },
+        exit: {
+            scale: 0.5,
+            opacity: 0
+        }
+    }
+
+    const title = {
+        init: {
+            color: "rgb(0,0,0)"
+        },
+        inView: {
+           color: "rgb(255, 255, 255)",
+           transition: {
+                duration: 1,
+                ease: "easeInOut"
+            }
+        },
+        exit: {
+            color: "rgb(0,0,0)"
+        }
+    }
+    
+    const cards = {
+        init: {
+            x: "200%"
+        },
+        inView: {
+            x: 0,
+            transition: {
+                duration: 1,
+                type: "spring",
+                ease: "easeInOut"
+            }
+        }
+    }
     return (
         <div id='experience' className={styles.excontainer}>
-            <h1 data-aos="zoom-out-right" className={styles.title}>Experience</h1>
-            <div className={styles.parts}>
-                <div data-aos="zoom-out-left" className={styles.card}>
-                    <div className={styles.imgContainer}>
-                        <Image loading="lazy" className={styles.img} src={ImgSrc} alt="amazon logo"/>
-                    </div>
-                    <div className={styles.info2}>
-                        <div className={styles.details}>
-                            <h1 className={styles.company}>Amazon</h1>
-                            <h2 className={styles.title_company}>Amazon ML Summer School&apos;22 - Apprenticeship</h2>
-                            <h2 className={styles.dates}>July 2022 - July 2022</h2>
-                            <Link className={styles.certificate} href="https://drive.google.com/file/d/1VjyrH3OTywtqrtzI6nQw_dlTfwBHJvpj/view" target="_blank">Certificate</Link>
+            <motion.div variants={main} initial="init" whileInView="inView" className={styles.background}>
+                <motion.div className={styles.titleCont}>
+                    <motion.h1 variants={title} className={styles.title}>Experience</motion.h1>
+                </motion.div>
+                <motion.div className={styles.cards}>
+                    <motion.div variants={cards} className={styles.card}>
+                        <div className={styles.info2}>
+                            <div className={styles.mainInfo}>
+                                <div className={styles.details}>
+                                    <h1 className={styles.company}>Amazon </h1>
+                                    <h2 className={styles.title_company}>Amazon ML Summer School&apos;22 - Apprenticeship</h2>
+                                    <div className={styles.detailsCont}>
+                                        <h2 className={styles.dates}>July 2022 - July 2022</h2>
+                                        <Link className={styles.certificate} href="https://drive.google.com/file/d/1VjyrH3OTywtqrtzI6nQw_dlTfwBHJvpj/view" target="_blank">Certificate</Link>
+                                    </div>
+                                </div>
+                                <div className={styles.imgContainer}>
+                                    <Image loading="lazy" className={styles.img} src={ImgSrc} alt="amazon logo"/>
+                                </div>
+                            </div>
+                            <div className={styles.about}>
+                                <p className={styles.points}>Discovered various machine learning techniques, including supervised
+            learning, unsupervised learning, and reinforcement learning.</p>
+                                <p className={styles.points}>Examine several ML algorithms, such as decision trees, linear regression,
+            and logistic regression.</p>
+                                <p className={styles.points}>Learn the fundamentals of deep neural networks, probability graphic
+            models, dimension reduction, and causal inference.</p>
+                            </div>
                         </div>
-                        <div className={styles.about}>
-                            <p className={styles.points}>Discovered various machine learning techniques, including supervised
-        learning, unsupervised learning, and reinforcement learning.</p>
-                            <p className={styles.points}>Examine several ML algorithms, such as decision trees, linear regression,
-        and logistic regression.</p>
-                            <p className={styles.points}>Learn the fundamentals of deep neural networks, probability graphic
-        models, dimension reduction, and causal inference.</p>
+                    </motion.div>
+                    <motion.div variants={cards} className={styles.card}>
+                        <div className={styles.info2}>
+                            <div className={styles.mainInfo}>
+                                <div className={styles.details}>
+                                    <h1 className={styles.company}>JP Morgan Chase&Co.</h1>
+                                    <h2 className={styles.title_company}>Software Engineering Virtual Experience</h2>
+                                    <div className={styles.detailsCont}>
+                                        <h2 className={styles.dates}>July 2022 - July 2022</h2>
+                                        <Link className={styles.certificate} href="https://drive.google.com/file/d/1aVG5rSuJZ6AqWcCH9JQFlQecG-_rjnKF/view?usp=share_link" target="_blank">Certificate</Link>
+                                    </div>
+                                </div>
+                                <div className={styles.imgContainer}>
+                                    <Image loading="lazy" className={styles.img} src={ImgSrc2} alt="jpmc logo"/>
+                                </div>
+                            </div>
+                            <div className={styles.about}>
+                                <p className={styles.points}>Recognize the principles of successful user experience design.</p>
+                                <p className={styles.points}>Possessed financial know-how that was matched with technical expertise to
+    effectively solve problems.</p>
+                                <p className={styles.points}>Gained practical experience using the perspective tool for data visualisation.</p>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div data-aos="zoom-out-left" className={styles.card}>
-                    <div className={styles.imgContainer}>
-                        <Image loading="lazy" className={styles.img} src={ImgSrc2} alt="jpmc logo"/>
-                    </div>
-                    <div className={styles.info2}>
-                        <div className={styles.details}>
-                            <h1 className={styles.company}>JP Morgan Chase&Co.</h1>
-                            <h2 className={styles.title_company}>Software Engineering Virtual Experience</h2>
-                            <h2 className={styles.dates}>July 2022 - July 2022</h2>
-                            <Link className={styles.certificate} href="https://drive.google.com/file/d/1aVG5rSuJZ6AqWcCH9JQFlQecG-_rjnKF/view?usp=share_link" target="_blank">Certificate</Link>
-                        </div>
-                        <div className={styles.about}>
-                            <p className={styles.points}>Recognize the principles of successful user experience design.</p>
-                            <p className={styles.points}>Possessed financial know-how that was matched with technical expertise to
-effectively solve problems.</p>
-                            <p className={styles.points}>Gained practical experience using the perspective tool for data visualisation.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                    </motion.div>
+                </motion.div>
+            </motion.div>
         </div>
     )
 }
--- components/projects.tsx
+++ components/projects.tsx
@@ -16,15 +16,25 @@ const Projects = () => {
     const hover = {
         init: {
             background: "rgb(255,255,255)",
-            color: "rgb(0,0,0)"
+            color: "rgb(0,0,0)",
+            scale: 1
         },
         animate: {
             background: "rgb(65, 105, 225)",
-            color: "rgb(255,255,255)"
+            color: "rgb(255,255,255)",
+            scale: 1.1,
+            transition: {
+                ease: "easeInOut",
+                duration: 0.5
+            }
         },
         exit: {
             background: "rgb(255,255,255)",
-            color: "rgb(0,0,0)"
+            color: "rgb(0,0,0)",
+            scale: 1,
+            transition: {
+                ease: "easeInOut"
+            }
         },
     }
 
@@ -106,69 +116,37 @@ const Projects = () => {
     return (
         <div id='projects' className={styles.container}>
             <motion.h1 variants={title} initial="init" whileInView="inView" exit="exit" className={styles.title}>Projects</motion.h1>
-            {/* <div className={styles.cont}> */}
-                <motion.div variants={list} initial="init" whileInView="inView" exit="exit" className={styles.projectsList}>
-                    {
-                        images.map((img, index) => {
-                            return (
-                                <Link href={`/${img.id}`} key={index} className={styles.link}>
-                                    <motion.div variants={liItem} initial="init" whileInView="inView" exit="exit" className={styles.projectCont}>
-                                    <motion.div variants={hover} initial="init" whileHover="animate" exit="exit" className={styles.project}>
-                                        <span className={styles.projtitle}>{img.name}</span>
-                                        <div className={styles.skills}>
-                                        {
-                                            img.tech.map((skill, ind) => {
-                                                return (
-                                                    <span key={ind} className={styles.skill}>{skill}</span>
-                                                )
-                                            })
-                                        }
-                                        </div>
-                                        <motion.div className={styles.arrow}>
-                                            <svg width="103" height="75" viewBox="0 0 103 75" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M1.5 37.5H101.5" stroke="black" stroke-width="3" stroke-linecap="round"/>
-                                                <path d="M66 2L101.355 37.3553" stroke="black" stroke-width="3" stroke-linecap="round"/>
-                                                <path d="M66 73L83.6777 55.3223L101.355 37.6447" stroke="black" stroke-width="3" stroke-linecap="round"/>
-                                            </svg>
-                                        </motion.div>
+            <motion.div variants={list} initial="init" whileInView="inView" exit="exit" className={styles.projectsList}>
+                {
+                    images.map((img, index) => {
+                        return (
+                            <Link href={`/${img.id}`} key={index} className={styles.link}>
+                                <motion.div variants={liItem} initial="init" whileInView="inView" exit="exit" className={styles.projectCont}>
+                                <motion.div variants={hover} initial="init" whileHover="animate" exit="exit" className={styles.project}>
+                                    <span className={styles.projtitle}>{img.name}</span>
+                                    <div className={styles.skills}>
+                                    {
+                                        img.tech.map((skill, ind) => {
+                                            return (
+                                                <span key={ind} className={styles.skill}>{skill}</span>
+                                            )
+                                        })
+                                    }
+                                    </div>
+                                    <motion.div className={styles.arrow}>
+                                        <svg width="103" height="75" viewBox="0 0 103 75" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M1.5 37.5H101.5" stroke="black" stroke-width="3" stroke-linecap="round"/>
+                                            <path d="M66 2L101.355 37.3553" stroke="black" stroke-width="3" stroke-linecap="round"/>
+                                            <path d="M66 73L83.6777 55.3223L101.355 37.6447" stroke="black" stroke-width="3" stroke-linecap="round"/>
+                                        </svg>
                                     </motion.div>
-                                    </motion.div>
-                                </Link>
-                            )
-                        })
-                    }
-                </motion.div>
-            {/* </div> */}
-            {/* <div className={styles.projects}>
-                <motion.div ref={carousel} className={styles.carousel}>
-                    <motion.div drag="x" dragConstraints={{ right: 0, left: -width }} className={styles.inner_carousel}>
-                        {
-                            images.map((img, index) => {
-                                return (
-                                    <motion.div key={index} className={styles.item}>
-                                        <img loading='lazy' className={styles.projimg} src={img.img[0]} alt='project photos' />
-                                        <div className={styles.info}>
-                                            <h1 className={styles.projtitle}>{img.name}</h1>
-                                            <div className={styles.techlist}>
-                                                {
-                                                    img.tech.map((t, index2) => {
-                                                        return(
-                                                            <div key={index2} className={styles.backtech}>
-                                                                <span>{t}</span>
-                                                            </div>
-                                                        )
-                                                    })
-                                                }
-                                            </div>
-                                            <h1 className={styles.getmore}><a className={styles.getmorelink} href={`/${img.id}`}>Get more information</a></h1>
-                                        </div>
-                                    </motion.div>
-                                )
-                            })
-                        }
-                    </motion.div>
-                </motion.div>      
-            </div> */}
+                                </motion.div>
+                                </motion.div>
+                            </Link>
+                        )
+                    })
+                }
+            </motion.div>
         </div>
     )
 }
